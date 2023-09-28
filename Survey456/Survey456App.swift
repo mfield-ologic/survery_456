5f8894465ecec7aa5fbdba7814afeaf73470f793
Michael Field <michaelcfield97@gmail.com>
2023-09-27 22:08:20 -0700

    Initial Commit

---
+++ Survey456/Survey456App.swift
@@ -0,0 +1,17 @@
+//
+//  Survey456App.swift
+//  Survey456
+//
+//  Created by Michael Field on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Survey456App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Survey456UITests/Survey456UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Survey456UITestsLaunchTests.swift
+//  Survey456UITests
+//
+//  Created by Michael Field on 9/27/23.
+//
+
+import XCTest
+
+final class Survey456UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
